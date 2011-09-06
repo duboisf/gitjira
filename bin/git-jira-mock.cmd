ddd55cd9a77494da9f6628a045b541faf70407ca
Bruno Navert <bruno.navert@gmail.com>
2011-09-05 22:29:11 -0400

    single line script

--- bin/git-jira-mock.cmd
+++ bin/git-jira-mock.cmd
@@ -3,5 +3,4 @@
 call scala -savecompiled -classpath "~drp0\gitjira.jar;~drp0\scarg.jar" %0 %*
 goto :eof
 ::!#
-Console.println("Hello, world!")
-argv.toList foreach Console.println
+(new gitjira.GitJiraMain with gitjira.DummyJiraProvider) main args
--- bin/git-jira-mock.sh
+++ bin/git-jira-mock.sh
@@ -2,5 +2,4 @@
 dir=$(dirname `readlink $0`)
 exec scala -savecompiled -classpath "$dir/scarg.jar:$dir/gitjira.jar" $0 $@
 !#
-import gitjira._
-(new GitJiraMain with DummyJiraProvider) main args
+(new gitjira.GitJiraMain with gitjira.DummyJiraProvider) main args
